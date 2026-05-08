aa11efe736a8940d0b06b25e3d1da314c8616a65
Manuella Avelino <ms8373630@gmail.com>
2026-05-08 17:44:54 -0300

    Add files via upload

---
+++ port.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+#-----Portifolio pessoal-----
+
+st.title('Portifólio Pessoal - Manuella Avelino')
+
+#-----Curiosidades-----
+
+st.header('Curiosidades')
+
+st.write('1. Sou apaixonada por Tecnologia e fiz curso técnico em Desenvolvimento de Sistemas e atualmente estou na faculdade cursando Análise e Desenvolvimento de Sistemas.')
+st.write('2. Já fui convocada para participar de uma olimpíada de programação no SENAI.')
+st.write('3. Tambem já fui convocada para apresentar o meu curso no SENAI em inglês para os executivos da empresa American Airline.')
+st.write('4. Sou apaixonada por cachorros.')
+st.write('5. Tenho uma cachorra chamada Layla, ela é uma Vira-Lata e tem 2 anos de idade.')
+
+
+#-----Imagem que me define-----
+
+st.header('Imagem que me define')
+
+st.image('https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/07/cachorros-caretas-inusitadas-fotografo-vieler.jpg?w=1200&h=675&crop=1', caption='Imagem que me define')
+
+
+#-----Link do meu GitHub-----
+
+st.header('Link do meu GitHub')
+
+st.write('Meu GitHub: [Manuella Avelino](https://github.com/Manucrellaa)')
+
