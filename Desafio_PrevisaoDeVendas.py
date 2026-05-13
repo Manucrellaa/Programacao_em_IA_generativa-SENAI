658970b1acbd80518a4287edb2c7aa338cd98d92
Manuella Avelino <ms8373630@gmail.com>
2026-05-13 16:35:36 -0300

    Add files via upload

---
+++ Desafio_PrevisaoDeVendas.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+st.header("Previsão de Vendas")
+
+# Dados: [Investimento em Marketing] -> Faturamento
+dados_vendas = pd.DataFrame({
+    'investimento': [100, 200, 300, 400, 500, 600],
+    'faturamento': [1200, 2500, 3200, 4800, 5100, 6300]
+})
+
+# objetivo: previsão de FATURAMENTO 
+
+st.scatter_chart(dados_vendas, x='investimento', y='faturamento')
+modelo_vendas = LinearRegression()
+modelo_vendas.fit(dados_vendas[['investimento']], dados_vendas['faturamento'])
+investimento_input = st.slider('Investimento em Marketing', 0, 1000, 300)
+faturamento_previsto = modelo_vendas.predict([[investimento_input]])
+st.metric('Faturamento Previsto', f'R$ {faturamento_previsto[0]:.2f}')
